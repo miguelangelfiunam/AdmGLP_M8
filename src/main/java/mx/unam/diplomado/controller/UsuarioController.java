1a58380645d7dc9d0e22107d8478d94e593ea633
miguelangel <miguelangel@dgp.unam.mx>
2022-02-15 23:29:59 -0600

    Adición de usuario
    Se agrega formulario para ingresar un usuario

--- src/main/java/mx/unam/diplomado/controller/UsuarioController.java
+++ src/main/java/mx/unam/diplomado/controller/UsuarioController.java
@@ -1,5 +1,8 @@
 package mx.unam.diplomado.controller;
 
+import java.util.List;
+import mx.unam.diplomado.forms.UsuarioForm;
+import mx.unam.diplomado.modelo.entidades.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import mx.unam.diplomado.service.UsuarioService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/usuarios")
@@ -15,9 +19,21 @@ public class UsuarioController {
     @Autowired
     UsuarioService usuarioService;
 
+    @ResponseBody
+    @RequestMapping(value = {"/jsonContacto"})
+    public List<Usuario> tipoContactoJson() {
+        return usuarioService.getUsuarios();
+    }
+
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
     public String cargaContacto(Model model) {
         model.addAttribute("usuarios", usuarioService.getUsuarios());
         return "usuarios";
+    }
+    
+    @RequestMapping("/usuarioForm")
+    public String usuarioForm(Model model) {
+        model.addAttribute("usuario", new UsuarioForm());
+        return "usuarioForm";
     }
 }
---
+++ src/main/java/mx/unam/diplomado/forms/UsuarioForm.java
@@ -0,0 +1,143 @@
+package mx.unam.diplomado.forms;
+
+import java.util.Date;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.NotBlank;
+
+/**
+ *
+ * @author unam
+ */
+public class UsuarioForm {
+
+    private Integer idUsuario; // Identificador de usuario
+
+    @Size(min = 5, max = 50)
+    private String apodo; // Seudonimo del usuario en la aplicacion
+
+    @NotNull
+    private String correo1; // Correo electronico
+
+    @NotBlank
+    private String correo2; // Correo electronico
+
+    @Size(max = 50)
+    @NotNull
+    private String nombre; // Nombre de la persona 50
+
+    @Size(max = 50)
+    @NotNull
+    private String apellido1; // Primer apellido 50
+
+    @Size(max = 50)
+    @NotNull
+    private String apellido2; // Segundo apellido 50
+
+    @NotNull
+    private Integer edad;// Edad de la persona
+
+    @NotNull
+    private Date fechaNacimiento; // Fecha de nacimiento
+
+    @NotNull
+    @Size(max = 10)
+    private String telefono1; // Telefono del usuario
+
+    @NotNull
+    @Size(max = 10)
+    private String telefono2; // Segundo telefono de contacto
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public String getCorreo1() {
+        return correo1;
+    }
+
+    public void setCorreo1(String correo1) {
+        this.correo1 = correo1;
+    }
+
+    public String getCorreo2() {
+        return correo2;
+    }
+
+    public void setCorreo2(String correo2) {
+        this.correo2 = correo2;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido1() {
+        return apellido1;
+    }
+
+    public void setApellido1(String apellido1) {
+        this.apellido1 = apellido1;
+    }
+
+    public String getApellido2() {
+        return apellido2;
+    }
+
+    public void setApellido2(String apellido2) {
+        this.apellido2 = apellido2;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono1() {
+        return telefono1;
+    }
+
+    public void setTelefono1(String telefono1) {
+        this.telefono1 = telefono1;
+    }
+
+    public String getTelefono2() {
+        return telefono2;
+    }
+
+    public void setTelefono2(String telefono2) {
+        this.telefono2 = telefono2;
+    }
+
+    @Override
+    public String toString() {
+        return "UsuarioForm{" + "idUsuario=" + idUsuario + ", apodo=" + apodo + ", correo1=" + correo1 + ", correo2=" + correo2 + ", nombre=" + nombre + ", apellido1=" + apellido1 + ", apellido2=" + apellido2 + ", edad=" + edad + ", fechaNacimiento=" + fechaNacimiento + ", telefono1=" + telefono1 + ", telefono2=" + telefono2 + '}';
+    }
+}
